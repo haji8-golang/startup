70e17eb06375d64f812b39789b8dcc1a6e7dd99d
haji8-golang <haji8.aistar@gmail.com>
2022-10-28 05:05:56 +0900

    ??

---
+++ 3.go and ga/ga.go
@@ -0,0 +1,43 @@
+package main
+
+
+import (
+    "html/template"
+    "net/http"
+    "google.golang.org/api/analytics/v3"
+)
+
+
+
+type Todo struct {
+    Title string
+    Done  bool
+}
+
+
+
+type TodoPageData struct {
+    PageTitle string
+    Todos     []Todo
+}
+
+
+
+func main() {
+    tmpl := template.Must(template.ParseFiles("layout.html"))
+    ctx := context.Background()
+    analyticsService, err := analytics.NewService(ctx)
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        data := TodoPageData{
+            PageTitle: "My TODO list",
+            Todos: []Todo{
+                {Title: "Task 1", Done: false},
+                {Title: "Task 2", Done: true},
+                {Title: "Task 3", Done: true},
+            },
+        }
+        tmpl.Execute(w, data)
+    })
+    http.ListenAndServe(":80", nil)
+}
+
